cf1b46d82deae7b9d9c9a9c3bad452ea0ef0eb42
nacukisubaru <incloudff@gmail.com>
2024-11-26 07:57:59 +0300

    bind

--- call-bind-apply-context/bind.js
+++ call-bind-apply-context/bind.js
@@ -3,12 +3,20 @@
 
 // Пример:
 
-// javascript
-// Копировать код
-// function greet(greeting, punctuation) {
-//   return `${greeting}, ${this.name}${punctuation}`;
-// }
+Object.prototype.myBind = function(context, ...args) {
+    if (typeof this !== "function") {
+        throw new Error('it is not a function');
+    }
 
-// const person = { name: 'John' };
-// const boundGreet = greet.myBind(person, 'Hello');
-// console.log(boundGreet('!')); // Hello, John!
+    return bound = (...boundArgs) => {
+        return this.apply(context, [...args, ...boundArgs]);
+    }
+}
+
+function greet(greeting = '', punctuation = '') {
+    return `${greeting}, ${this.name}${punctuation}`;
+}
+
+const person = { name: 'John' };
+const boundGreet = greet.myBind(person, 'Hello');
+console.log(boundGreet('!')); // Hello, John!
