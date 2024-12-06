b8a6375a21556b3881f0081f838a00b5de4b75bb
Alexander Krestinskiy <incloudff@gmail.com>
2024-12-06 22:56:50 +0300

    maxDeep решение задачи

--- arrays-objects/maxDeep.js
+++ arrays-objects/maxDeep.js
@@ -4,29 +4,55 @@
 // Узел дерева имеет два дочерних элемента: left и right.
 // Каждый узел может быть либо числом (значение узла), либо null (отсутствие дочернего элемента).
 // Пример структуры дерева:
-// javascript
-// Копировать код
-// const tree = {
-//   value: 1,
-//   left: {
-//     value: 2,
-//     left: {
-//       value: 4,
-//       left: null,
-//       right: null
-//     },
-//     right: null
-//   },
-//   right: {
-//     value: 3,
-//     left: null,
-//     right: {
-//       value: 5,
-//       left: null,
-//       right: null
-//     }
-//   }
-// };
+
+
+const tree = {
+	value: 1,
+	left: {
+		value: 2,
+		left: {
+			value: 4,
+			left: {
+				value: 5,
+				left: null,
+				right: {
+					value: 5,
+					left: null,
+					right: null
+				}
+			},
+			right: null
+		},
+		right: null
+	},
+	right: {
+		value: 3,
+		left: {
+			value: 5,
+			left: null,
+			right: null
+		},
+		right: {
+			value: 5,
+			left: null,
+			right: null
+		}
+	},
+	right2: {
+		value: 3,
+		left: {
+			value: 5,
+			left: null,
+			right: null
+		},
+		right: {
+			value: 5,
+			left: null,
+			right: null
+		}
+	}
+};
+
 // Задача:
 // Написать функцию, которая принимает дерево в виде объекта и возвращает наибольшую глубину дерева.
 
@@ -38,3 +64,17 @@
 // getMaxDepth(tree); // 3
 // Подсказка:
 // Наибольшая глубина — это максимальное число узлов на пути от корня до самого дальнего листа.
+
+function maxDeep(object) {
+	let maxDeepCounter = 0;
+	Object.values(object).forEach((item) => {
+		if (item && typeof item === "object") {
+			maxDeepCounter++;
+			maxDeepCounter += maxDeep(item)
+		}
+	});
+
+	return maxDeepCounter;
+}
+
+console.log(maxDeep(tree));
